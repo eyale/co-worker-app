80543fed597ccbbc08b5b8eeb4697e9b2743c3e0
Anton Honcharov <4eyalush@gmail.com>
2017-02-11 18:11:30 +0200

    adding the semantic ui react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FilterLink from './FilterLink.jsx';
+import { Button } from 'semantic-ui-react'
 
 import styles from './Footer.less';
 
@@ -8,6 +9,9 @@ export default class Footer extends Component {
     render() {
         return (
             <div className={styles.root}>
+              <Button>
+               Click Here
+              </Button>
                 <FilterLink filter="SHOW_ALL">All</FilterLink>
                 <FilterLink filter="SHOW_NEW">New</FilterLink>
                 <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
